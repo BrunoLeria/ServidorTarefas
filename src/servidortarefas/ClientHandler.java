0d4751a80bd28db895be94e5cd44f602b3962963
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2022-11-02 18:27:59 -0300

    Servidor responde aos comandos do cliente

--- src/servidortarefas/ClientHandler.java
+++ src/servidortarefas/ClientHandler.java
@@ -6,6 +6,7 @@ package servidortarefas;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -30,13 +31,26 @@ class ClientHandler implements Runnable {
         try {
             System.out.println("servidortarefas.ClientHandler.run()");
             Scanner entradaCliente = new Scanner(socket.getInputStream());
+            PrintStream saidaCliente = new PrintStream(socket.getOutputStream());
 
             while (entradaCliente.hasNextLine()) {
                 String comando = entradaCliente.nextLine();
                 System.out.println(comando);
+                switch (comando) {
+                    case "c1":
+                        saidaCliente.println("Confirmação comando c1.");
+                        break;
+                    case "c2":
+                        saidaCliente.println("Confirmação comando c2.");
+                        break;
+                    default:
+                        saidaCliente.println("Comando não encontrado.");
+                        break;
+                }
             }
 
             entradaCliente.close();
+            saidaCliente.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
